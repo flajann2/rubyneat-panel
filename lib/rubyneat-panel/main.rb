99f64b353d3a958848c12c1ed172ade240c48875
Fred Mitchell <fred.mitchell@gmx.de>
2016-11-20 14:07:54 +0100

    fox working

--- lib/rubyneat-panel/main.rb
+++ lib/rubyneat-panel/main.rb
@@ -1,17 +1,28 @@
 module RubyNEAT
   module Panel
     def self.launch
+      main = Main.new
+      Panel.app.create
+      Panel.app.run
     end
     
     def self.test
-      main = FXMainWindow.new(app, "RubyNEAT Dashboard", nil, nil, DECOR_NONE)
-      FXButton.new(main, "&RubyNEAT Panel Test!", nil, app, FXApp::ID_QUIT)
-      app.create()
+      main = FXMainWindow.new(Panel.app, "RubyNEAT Dashboard", nil, nil, DECOR_NONE)
+      FXButton.new(main, "&RubyNEAT Panel Test!", nil, Panel.app, FXApp::ID_QUIT)
+      Panel.app.create()
       main.show(PLACEMENT_SCREEN)
-      app.run()
+      Panel.app.run()
     end
     
     class Main < FXMainWindow
+      def initialize
+        super(Panel.app, "RubyNEAT Panel", width: 700, height: 400)
+      end
+
+      def create
+        super
+        show(PLACEMENT_SCREEN)
+      end
     end
   end
 end
