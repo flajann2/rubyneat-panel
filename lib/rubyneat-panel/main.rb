7160057f05e2931e3bb471d2b11047aeaab99ac9
Fred Mitchell <fred.mitchell@gmx.de>
2016-12-05 14:55:15 +0100

    changed the name of th executable to neat-panel

--- lib/rubyneat-panel/main.rb
+++ lib/rubyneat-panel/main.rb
@@ -13,11 +13,35 @@ module RubyNEAT
       app_name "RubyNEAT Panel"
       vendor_name "www.rubyneat.de"
       
+      fx_tool_tip {}
+      
       fx_main_window :main do
         title "RubyNEAT Panel"
         width 700
         height 400
         instance { |mw| mw.show PLACEMENT_SCREEN }
+
+        #fx_menu_bar { opts LAYOUT_SIDE_TOP|LAYOUT_FILL_X }
+        fx_horizontal_separator { opts LAYOUT_SIDE_TOP|LAYOUT_FILL_X|SEPARATOR_GROOVE }
+        fx_horizontal_frame do
+          opts LAYOUT_SIDE_TOP|FRAME_NONE|LAYOUT_FILL_X|LAYOUT_FILL_Y|PACK_UNIFORM_WIDTH
+          fx_tab_book do
+            fx_tab_item { text "&Overview" }
+            fx_horizontal_frame {
+              opts FRAME_THICK|FRAME_RAISED
+              fx_list {
+                opts LIST_EXTENDEDSELECT|LAYOUT_FILL_X|LAYOUT_FILL_Y
+                instance { |list|
+                  list.appendItem "First"
+                  list.appendItem "Second"
+                  list.appendItem "Third"
+                }
+              }
+            }
+            fx_tab_item { text "&Stats" }
+            fx_tab_item { text "&Critter" }
+          end
+        end
       end
     end
   end
